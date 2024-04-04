20867b4639e4acb43515c093f54b7e808d07fc83
yasinitskyi <yuralee007@gmail.com>
2024-04-04 21:24:35 +0800

    feat: Search page for "search" route

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { PhoneInput } from "@/components/Input";
+
+export default function Search() {
+  return (
+    <>
+        <PhoneInput isError={false} onChange={() => {}} />
+    </>
+  );
+}

b1c4498e1e7a51905e3a82229b3d6cc92ad7a0dc
yasinitskyi <yuralee007@gmail.com>
2024-04-06 00:26:02 +0800

    feat: AlphanumericInput & NameInput reimport

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,11 @@
+import AlphanumericInput from "./AlphanumericInput";
 import GenericInput from "./GenericInput";
 import PhoneInput from "./PhoneInput";
+import NameInput from "./NameInput";
 
 export {
+    AlphanumericInput,
     GenericInput,
     PhoneInput,
+    NameInput,
 }

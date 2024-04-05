385db7537a89e698a25d11d9cf6e60cc0a49787f
yasinitskyi <yuralee007@gmail.com>
2024-04-06 01:10:45 +0800

    progress: ReadonlyInput part of Search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { PhoneInput, NameInput, AlphanumericInput } from "@/components/Input";
+import { PhoneInput, NameInput, AlphanumericInput, ReadonlyInput } from "@/components/Input";
 import { replaceNonNumeric, processECJK, replaceNonAlphanumeric } from "@/utilities/stringProcessors";
 import Styles from './index.module.css';
 
 export default function Search() {
   return (
     <form className={Styles.form}>
+      <ReadonlyInput name="airport details" title="下車機場" value="桃園國際機場 第一航廈" />
       <AlphanumericInput 
         name="flight number" 
         title="航班編號" 

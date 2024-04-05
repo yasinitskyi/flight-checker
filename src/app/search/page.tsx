ad9c5e0ace59b3f8b72993ecd1e800b9a88d148a
yasinitskyi <yuralee007@gmail.com>
2024-04-06 00:25:04 +0800

    progress: form as part of Search & AlphanumericInput as part of form

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,26 @@
 "use client"
 
-import { PhoneInput, NameInput } from "@/components/Input";
+import { PhoneInput, NameInput, AlphanumericInput } from "@/components/Input";
+import { replaceNonNumeric, processECJK, replaceNonAlphanumeric } from "@/utilities/stringProcessors";
+import Styles from './index.module.css';
 
 export default function Search() {
   return (
-    <>
-        <PhoneInput />
-        <NameInput />
-    </>
+    <form className={Styles.form}>
+      <AlphanumericInput 
+        name="flight number" 
+        title="航班編號" 
+        placeholder="e.g. BR003" 
+        processor={replaceNonAlphanumeric} 
+      />
+      <PhoneInput processor={replaceNonNumeric} />
+      <NameInput processor={processECJK} />
+      <AlphanumericInput 
+        name="document details" 
+        title="身份證字號/護照編號" 
+        placeholder="e.g. A123456789" 
+        processor={replaceNonAlphanumeric} 
+      />
+    </form>
   );
 }

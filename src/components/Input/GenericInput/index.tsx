b0117c84c8a022cd9651bf2cbc77eee3bd04c08a
yasinitskyi <yuralee007@gmail.com>
2024-04-05 15:42:48 +0800

    progress: GenericInput validation polishing & props

--- src/components/Input/GenericInput/index.tsx
+++ src/components/Input/GenericInput/index.tsx
@@ -1,29 +1,50 @@
 "use client"
 
+import {ChangeEvent, useState} from 'react';
 import Styles from './index.module.css';
-import { GenericInputProps } from '../InputTypes';
+import { GenericInputProps } from '../inputTypes';
 import classNames from 'classnames';
 
 function GenericInput({
     title,
     placeholder,
-    value,
-    isError = false,
+    value: defaultValue = '',
+    isError: defaultIsError = false,
     onChange = () => {},
     name,
+    pattern,
+    type,
+    validator,
 }: GenericInputProps) {
-  return (
+    const [value, _setValue] = useState(defaultValue);
+    const [isError, setIsError] = useState(defaultIsError);
+
+    const validate = (event: ChangeEvent<HTMLInputElement>) => {
+        if (pattern) setIsError(event.target.validity.patternMismatch);
+        const newValue = validator?.(event.target.value) || event.target.value;
+
+        setValue(newValue);
+    }
+
+    const setValue = (newValue: string) => {
+        _setValue(newValue);
+        onChange(newValue);
+    }
+
+    return (
     <div className={Styles.container}>
         {title && <label className={Styles.label} htmlFor={name}>{title}</label>}
-        <input
+        <input 
+            title={`請輸入您的${title}`}
             placeholder={placeholder}
             name={name}
             id={name}
             required 
             value={value} 
-            type="text" 
-            onChange={(event) => onChange(event.target.value)}
+            type={type || 'text'}
+            onChange={(event) => validate(event) }
             className={classNames(Styles.input, {[Styles.error]: isError})}
+            pattern={pattern}
         />
     </div>
   )

f5d743bb98b8fce4768cd3d1fd9d4bae57916bdd
yasinitskyi <yuralee007@gmail.com>
2024-04-06 00:20:25 +0800

    chore: PhoneInput validator prop name to processor

--- src/components/Input/PhoneInput/index.tsx
+++ src/components/Input/PhoneInput/index.tsx
@@ -4,19 +4,19 @@ import { GenericInput } from '../';
 
 export default function PhoneInput({
     value,
-    isError,
     onChange,
+    processor,
 }: InputProps) {
   return (
     <GenericInput 
-        title="電話" 
-        name="phone" 
-        placeholder="e.g. 0908000000" 
-        onChange={onChange} 
-        isError={isError} 
+        title="電話"
+        name="phone"
+        placeholder="e.g. 0908000000"
         value={value}
         pattern="09[0-9]{8}"
         type="tel"
+        onChange={onChange}
+        processor={processor}
     />
   )
 }

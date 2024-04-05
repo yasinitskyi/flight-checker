45b347ecb6fb87461be77e04f6dc973638141cfa
yasinitskyi <yuralee007@gmail.com>
2024-04-05 15:37:57 +0800

    chore: GenericInput props replacement

--- src/components/Input/PhoneInput/index.tsx
+++ src/components/Input/PhoneInput/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { PhoneInputProps } from '../InputTypes';
+import { InputProps } from '../inputTypes';
 import { GenericInput } from '../';
 
 export default function PhoneInput({
     value,
     isError,
     onChange,
-}: PhoneInputProps) {
+}: InputProps) {
   return (
-    <GenericInput title="電話" name="phone" placeholder="e.g. 0908000000" onChange={onChange} isError={isError} value={value} />
+    <GenericInput 
+        title="電話" 
+        name="phone" 
+        placeholder="e.g. 0908000000" 
+        onChange={onChange} 
+        isError={isError} 
+        value={value}
+        pattern="09[0-9]{8}"
+        type="tel"
+    />
   )
 }

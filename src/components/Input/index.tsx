48a5bfe4713de53fdaa3e00dd467bb304a9074ec
yasinitskyi <yuralee007@gmail.com>
2024-04-06 01:11:39 +0800

    feat: ReadonlyInput reimport

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,9 +2,11 @@ import AlphanumericInput from "./AlphanumericInput";
 import GenericInput from "./GenericInput";
 import PhoneInput from "./PhoneInput";
 import NameInput from "./NameInput";
+import ReadonlyInput from "./ReadonlyInput";
 
 export {
     AlphanumericInput,
+    ReadonlyInput,
     GenericInput,
     PhoneInput,
     NameInput,

1c8194a03ac194ee551309f8ca5bd930d7a1587f
yasinitskyi <yuralee007@gmail.com>
2024-04-06 00:21:16 +0800

    chore: NameInput validator prop name to processor & isError prop deleted

--- src/components/Input/NameInput/index.tsx
+++ src/components/Input/NameInput/index.tsx
@@ -4,8 +4,8 @@ import { GenericInput } from '../';
 
 export default function NameInput({
     value,
-    isError,
     onChange,
+    processor,
 }: InputProps) {
   return (
     <GenericInput 
@@ -13,9 +13,9 @@ export default function NameInput({
         name="full name" 
         placeholder="e.g. Ming Hsio" 
         onChange={onChange} 
-        isError={isError} 
         value={value}
         pattern="[\u4e00-\u9fa5a-zA-Z\s]*"
+        processor={processor}
     />
   )
 }

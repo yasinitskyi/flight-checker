5c146c26aae10407f96f6a30dbd0add7ba3b0ab3
yasinitskyi <yuralee007@gmail.com>
2024-04-06 01:09:49 +0800

    feat: ReadonlyInpuProps & readonly field as part of GenericInputProps interface

--- src/components/Input/InputTypes/index.ts
+++ src/components/Input/InputTypes/index.ts
@@ -7,9 +7,11 @@ export interface GenericInputProps {
     placeholder?: string,
     pattern?: string,
     type?: HTMLInputTypeAttribute,
+    readonly?: boolean,
     onChange?: (value: string) => void,
     processor?: (value: string) => string,
 }
 
 export type InputProps = Pick<GenericInputProps, 'value' | 'onChange' | 'processor'>;
 export type AlphaNumericInputProps = Omit<GenericInputProps, 'pattern' | 'type'>;
+export type ReadonlyInputProps = Pick<GenericInputProps, 'name' | 'value' | 'title'>

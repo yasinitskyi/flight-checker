5154359ba6fdb98f8f496c3716a83823aba67327
yasinitskyi <yuralee007@gmail.com>
2024-04-06 13:32:40 +0800

    progress: Typography as part of Search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,18 +2,26 @@
 
 import { PhoneInput, NameInput, AlphanumericInput, ReadonlyInput } from "@/components/Input";
 import { replaceNonNumeric, processECJK, replaceNonAlphanumeric } from "@/utilities/stringProcessors";
+import Typography from "@/components/Typography";
 import Styles from './index.module.css';
 
 export default function Search() {
   return (
     <form className={Styles.form}>
-      <ReadonlyInput name="airport details" title="下車機場" value="桃園國際機場 第一航廈" />
+      <Typography tag="h1">送機行程</Typography>
+      <Typography className={Styles.subTitle} tag="h2">送機計劃</Typography>
+      <ReadonlyInput 
+        name="airport details" 
+        title="下車機場" 
+        value="桃園國際機場 第一航廈" 
+      />
       <AlphanumericInput 
         name="flight number" 
         title="航班編號" 
         placeholder="e.g. BR003" 
         processor={replaceNonAlphanumeric} 
       />
+      <Typography className={Styles.subTitle} tag="h2">旅客資訊</Typography>
       <PhoneInput processor={replaceNonNumeric} />
       <NameInput processor={processECJK} />
       <AlphanumericInput 

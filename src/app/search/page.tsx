ec0a5d431bb91fdaeb37eba26e9bf02a63a7f327
yasinitskyi <yuralee007@gmail.com>
2024-04-06 22:17:57 +0800

    progress: ResponsiveGrid as part of Search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,6 +6,7 @@ import { replaceNonNumeric, processECJK, replaceNonAlphanumeric } from "@/utilit
 import Textarea from "@/components/Textarea";
 import Typography from "@/components/Typography";
 import { DarkButton } from "@/components/Button";
+import ResponsiveGrid from "@/components/ResponsiveGrid";
 import Styles from './index.module.css';
 
 export default function Search() {
@@ -17,26 +18,30 @@ export default function Search() {
     <form onSubmit={handleSubmit} className={Styles.form}>
       <Typography tag="h1">送機行程</Typography>
       <Typography className={Styles.subTitle} tag="h2">送機計劃</Typography>
-      <ReadonlyInput 
-        name="airport details" 
-        title="下車機場" 
-        value="桃園國際機場 第一航廈" 
-      />
-      <AlphanumericInput 
-        name="flight number" 
-        title="航班編號" 
-        placeholder="e.g. BR003" 
-        processor={replaceNonAlphanumeric} 
-      />
+      <ResponsiveGrid>
+        <ReadonlyInput 
+          name="airport details" 
+          title="下車機場" 
+          value="桃園國際機場 第一航廈" 
+        />
+        <AlphanumericInput 
+          name="flight number" 
+          title="航班編號" 
+          placeholder="e.g. BR003" 
+          processor={replaceNonAlphanumeric} 
+        />
+      </ResponsiveGrid>
       <Typography className={Styles.subTitle} tag="h2">旅客資訊</Typography>
-      <PhoneInput processor={replaceNonNumeric} />
-      <NameInput processor={processECJK} />
-      <AlphanumericInput 
-        name="document details" 
-        title="身份證字號/護照編號" 
-        placeholder="e.g. A123456789" 
-        processor={replaceNonAlphanumeric} 
-      />
+      <ResponsiveGrid>
+        <PhoneInput processor={replaceNonNumeric} />
+        <NameInput processor={processECJK} />
+        <AlphanumericInput 
+          name="document details" 
+          title="身份證字號/護照編號" 
+          placeholder="e.g. A123456789" 
+          processor={replaceNonAlphanumeric} 
+        />
+      </ResponsiveGrid>
       <Textarea
         title="乘車備註" 
         name="additional notes" 

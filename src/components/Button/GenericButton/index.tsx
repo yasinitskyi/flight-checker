0111bafc3e729b4aca4b6758cd551c8d7d9f7927
yasinitskyi <yuralee007@gmail.com>
2024-04-06 16:43:37 +0800

    feat: GenericButton component

---
+++ src/components/Button/GenericButton/index.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Styles from './index.module.css';
+import classNames from "classnames";
+import { GenericButtonProps } from "../buttonTypes";
+
+export default function GenericButton({
+  value,
+  theme,
+  children,
+  form = '',
+  disabled = false,
+  type = "button",
+  onClick = () => {},
+}: GenericButtonProps) {
+  return (
+    <div className={Styles.container}>
+      <button
+        {...(form ? {form} : {})}
+        className={classNames(Styles.button, Styles[theme])}
+        type={type} 
+        value={value}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
+  )
+}

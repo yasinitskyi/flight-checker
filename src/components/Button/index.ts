85144a1fef62ced2552d816bc67924923f5f9f12
yasinitskyi <yuralee007@gmail.com>
2024-04-06 16:48:48 +0800

    feat: button components reimports

---
+++ src/components/Button/index.ts
@@ -0,0 +1,9 @@
+import GenericButton from "./GenericButton";
+import DarkButton from "./DarkButton";
+import LightButton from "./LightButton";
+
+export {
+	GenericButton,
+	DarkButton,
+	LightButton,
+}

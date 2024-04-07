59863482423210cf4c428b15b11a495a6da6054d
yasinitskyi <yuralee007@gmail.com>
2024-04-07 14:03:42 +0800

    feat: API class definition & getFlights async method

---
+++ src/api/index.ts
@@ -0,0 +1,19 @@
+import { FlightInformation, FlightInformationProps } from "@/models/flightInformation";
+
+class API {
+	async getFlights(): Promise<FlightInformation[] | undefined> {
+		try {
+			const flights = await fetch('https://tdx.tansportdata.tw/api/basic/v2/Air/FIDS/Airport/Departure/TPE?$orderby=ScheduleDepartureTime&$format=JSON');
+    	const parsedFlights = await flights.json();
+
+			return parsedFlights.map((flight: FlightInformationProps) => new FlightInformation(flight));
+		} catch (error) {
+			console.error('There was an error: ', error);
+			alert('Error, try again later');
+		}
+	}
+}
+
+const api = new API();
+
+export {api, API};

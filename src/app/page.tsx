b4cc4ca3111df1d8d709b77e1a8c834f794b5da0
yasinitskyi <yuralee007@gmail.com>
2024-04-07 14:04:41 +0800

    feat: fake route redirect & dummy app logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Image from 'next/image'
+import Styles from "./page.module.css";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/search');
+  }, []);
+
   return (
-    <main className={styles.main}>
-      You are on main page
+    <main className={Styles.main}>
+      <div className={Styles.imageContainer}>
+        <Image src="/taxilogo.png" alt="Application Taxi Logo" fill />
+      </div>
     </main>
   );
 }
